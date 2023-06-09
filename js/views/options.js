06fee4fee975ec7aceb2ac20f974b35a31768c53
hansanhha <kwixs35@gmail.com>
2023-06-09 17:46:45 +0900

    js/views 파일 및 style.css 추가, 기본 index.html

---
+++ js/views/options.js
@@ -0,0 +1,27 @@
+export function surname() {
+
+}
+
+export function gender() {
+
+}
+
+export function nameType() {
+
+}
+
+export function requiredName() {
+
+}
+
+export function nameNumber() {
+
+}
+
+export function namePrefer() {
+
+}
+
+export function specification() {
+
+}
