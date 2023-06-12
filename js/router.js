35e2b420b2cc875020b95c5b2440c3a27dbd11bd
hansanhha <kwixs35@gmail.com>
2023-06-12 20:07:41 +0900

    router 파일 수정

--- js/router.js
+++ js/router.js
@@ -1,35 +1,49 @@
-import { home } from "./component/home.js";
-import { optionList } from "./component/options.js";
-import { result } from "./component/result.js";
-import { notFound } from "./component/notFound.js";
+import { Home } from "./component/home.js";
+import { Option } from "./component/options.js";
+import { Generate } from "./component/generating.js";
+import { Result } from "./component/result.js";
+import { NotFound } from "./component/notFound.js";
+import { setSelectOptions } from "./app.js";
+
+let currentComponent;
 
 const routes = [
-    { path: '/', component: home },
-    { path: '/option', component: optionList },
-    { path: '/result', component: result },
+    { path: '/', component: Home },
+    { path: '/option', component: Option },
+    { path: '/generating', component: Generate },
+    { path: '/result', component: Result },
 ]
 
 function findComponent(_pathname) {
     const pathname = _pathname ?? window.location.pathname;
     
-    return routes.find(route => route.path == pathname)?.component || notFound;
+    return routes.find(route => route.path == pathname)?.component || NotFound;
 }
 
-function render(app, pathname, props) {
+export function render(app, pathname) {
     const component = findComponent(pathname);
-    app.innerHTML = component(props);
+    currentComponent = new component();
+
+    app.innerHTML = currentComponent.getView();
+    currentComponent.configureEvent();
 }
 
-function historyPush(pathname) {
-    window.history.pushState(null, null, window.location.origin + pathname);
+export function elementRender(app) {
+    app.innerHTML = currentComponent.getView();
+
+    currentComponent.configureEvent();
 }
 
-function initRender(app) {
+export function prevElementRender(app) {
+    app.innerHTML = currentComponent.getPreView();
+
+    currentComponent.configureEvent();
+}
+
+export function initRender(app) {
     render(app, '/');
 }
 
-
-export { render, historyPush, initRender };
 
 
 
