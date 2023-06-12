6bed58a3f08d0ef1cc26a2fddd9f501d690f478e
hansanhha <kwixs35@gmail.com>
2023-06-12 20:09:27 +0900

    app.js 수정

--- js/App.js
+++ js/App.js
@@ -1,11 +1,23 @@
 import { initRender } from './router.js';
-import { historyPushEvent, historyPopEvent } from './historyEvent.js';
+import { nextView } from './event.js';
 
 const $app = document.querySelector('#app');
+export let selectOptions;
+export let generatedNames;
+
+export function setSelectOptions(options) {
+    selectOptions = options;
+    console.log(selectOptions);
+}
+
+export function setGeneratedNames(names) {
+    generatedNames = names;
+    console.log(generatedNames);
+} 
 
 initRender($app);
-historyPushEvent($app);
-historyPopEvent($app);
+nextView();
+
 
 
 
