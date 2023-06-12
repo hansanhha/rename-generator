6c2463e7931f7d116d4a7d26ccbae01cf603dd9c
hansanhha <kwixs35@gmail.com>
2023-06-12 20:04:29 +0900

    dom event 파일 분리 (event.js)

---
+++ js/event.js
@@ -0,0 +1,49 @@
+import { render, elementRender, prevElementRender } from './router.js';
+
+const $app = document.querySelector('#app');
+
+export function nextView() {
+    const links = document.querySelectorAll('a.link');
+
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault(); 
+            const pathname = e.target.getAttribute('href');
+            historyPush(pathname);
+            render($app, pathname);
+        });
+    });
+}
+
+function historyPush(pathname) {
+    window.history.pushState(null, null, window.location.origin + pathname);
+}
+
+export function nextElement() {
+    const links = document.querySelectorAll('button.link');
+
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault(); 
+            elementRender($app);
+        });
+    });
+}
+
+export function prevElement() {
+    const links = document.querySelectorAll('button.prev');
+
+    links.forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault(); 
+            prevElementRender($app);
+        });
+    });
+}
+
+export function previousView() {
+
+    window.addEventListener('popstate', () => {
+        render($app);
+    })
+}
