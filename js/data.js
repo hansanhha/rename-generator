7028f16a42f10a392b7377075f10fa019382749c
hansanhha <kwixs35@gmail.com>
2023-06-14 13:44:03 +0900

    전역변수 app, result 생성

--- js/data.js
+++ js/data.js
@@ -1,25 +1,58 @@
 /**
  * 애플리케이션 전역 변수
  * selectOptions: 사용자가 입력한 값을 가진 객체
- * generatedNames: 이름 객체 배열
+ * generatedNames: Name 객체 배열
  * Name: api 요청으로 받은 데이터를 토대로 만든 이름 객체
  */
 
-export let selectOptions;
-export let generatedNames = [];
+export const $app = document.querySelector('#app');
 
-export class Name {
+export let selectOptions;
+
+class Result {
+
+    generatedNames = [];
+    message = '';
+    ps = '';
+
+    /**
+     * 
+     * @param {JSON} nameDict: gpt 답변 객체 (name, value, other)
+     */
+    // Name 객체 생성 후 멤버 변수 배열에 삽입
+    push(nameDict) {
+        const name = new Name(nameDict.name, nameDict.value);
+
+        // gpt가 동명이인 유명인물(other)을 소개를 안해주는 경우도 있음
+        if (nameDict.other !== '') {
+            name.setOther(nameDict.other);
+        }
+        this.generatedNames.push(name);
+    }
+
+    setMessage(message) {
+        this.message = message;
+    }
+
+    setPS(ps) {
+        this.ps = ps;
+    }
+}
+
+class Name {
 
     constructor(name, comment) {
         this.name = name;
         this.comment = comment;
+        this.other = '';
     }
 
-    setPerson(person) {
-        this.person = person;
+    setOther(other) {
+        this.other = other;
     }
 }
 
+export const result = new Result();
 
 export function setSelectOptions(options) {
     selectOptions = options;
@@ -27,6 +60,5 @@ export function setSelectOptions(options) {
 }
 
 export function setGeneratedNames(names) {
-    generatedNames = names;
-    console.log(generatedNames);
+    generatedNames.push(names);
 }
