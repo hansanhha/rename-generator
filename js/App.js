8054c2c8426c29d025895c6cf4b1bb50c87f8daa
hansanhha <kwixs35@gmail.com>
2023-06-14 13:44:51 +0900

    전역변수  참조 수정 및 주석 추가

--- js/App.js
+++ js/App.js
@@ -1,14 +1,17 @@
 import { initRender } from './router.js';
 import { nextView, previousView } from './event.js';
-
-const $app = document.querySelector('#app');
+import { $app } from './data.js';
 
 initRender($app);
 nextView();
 previousView();
 
-// 전체 흐름 router -> component -> element
-// gpt 사용 : generating component -> request.js -> result component
+// router: path마다 render 담당
+// element[props]: 순수 html 요소 반환 (template literal) 
+// component: element + 추가 요소(기능) 반환
+
+// 전체 흐름 button -> router(component,element) -> render 
+// gpt 사용: generating component -> request.js -> result component
 
 
 
