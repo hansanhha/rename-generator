3c3539e423246cad22a8a51835c8454fca6613a1
hansanhha <kwixs35@gmail.com>
2023-06-15 09:09:14 +0900

    result element 내용 추가

--- js/element/result.js
+++ js/element/result.js
@@ -1,5 +1,7 @@
 export function resultElement(props) {
     return `
-        
+        <p>추천 이름 : ${props.name}</p>
+        <p>이름의 의미 : ${props.value}</p>
+        <p>gpt의 생각 : ${props.reason}</p>
     `
 }
