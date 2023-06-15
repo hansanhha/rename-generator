6a3b40281790467dcb97b8c713ff9b318ea0138c
hansanhha <kwixs35@gmail.com>
2023-06-15 09:12:47 +0900

    generating gpt 요청 메시지 수정

--- js/component/generating.js
+++ js/component/generating.js
@@ -28,17 +28,15 @@ export class Generate extends Component{
             이제 이름을 만들어줘.
             요구사항은 다음과 같아.
             1. '${selectOptions.surname}'씨 성과 어울리는 이름을 추천해줘
-            2. 성별은 ${selectOptions.gender}에 어울리는 이름을 추천해줘
+            2. ${selectOptions.gender}에 어울리는 이름을 추천해줘
             3. ${selectOptions.name_type}으로 추천해줘
             4. ${selectOptions.nameNumber} 글자 이름을 이름을 추천해줘
             5. ${selectOptions.prefer_name_category}으로 추천해줘
-
-            한 번 더 설명하는데, 요구사항을 모두 만족한 이름을 지어서 총 5가지의 이름을 추천해줘
-            그리고 이름을 지을 때는 위에서 설명한 것들을 참고해서 지어줘.
+            6. 결과는 문장은 제외하고 json으로만 해줘
         `;
         if (selectOptions.requiredLetter !== '') {
             message += `
-                4. '${selectOptions.requiredLetter}'이라는 글자가 이름에 꼭 들어갔으면 좋겠어
+                7. '${selectOptions.requiredLetter}'이라는 글자가 이름에 꼭 들어갔으면 좋겠어
             `
         }
         return message;
